5f96eb51ab8ff919a0d5e42c9d6e013ebf9c3268
Erik (Lurch) <fat.lurch@gmail.com>
2021-10-30 18:53:01 -0700

    Update Serial_Read.py

--- Serial_Read.py
+++ Serial_Read.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import time
 import serial
-import unicodedata
 ser = serial.Serial(
 port='/dev/serial0',
 baudrate = 9600,
@@ -9,7 +8,6 @@ parity=serial.PARITY_NONE,
 stopbits=serial.STOPBITS_ONE,
 bytesize=serial.EIGHTBITS,
 )
-
 
 while 1:
     x=ser.readline()
